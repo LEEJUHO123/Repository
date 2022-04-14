597cd8d09a929af36b5985f79e0f40185506b0f8
JUHO <JUHO@DESKTOP-2A8GO7K>
2022-04-14 18:53:36 +0900

    0413

---
+++ P20220413/src/co/edu/ReferenceExamAlone.java
@@ -0,0 +1,23 @@
+package co.edu;
+
+public class ReferenceExamAlone {
+	public static void main(String[] args) {
+		
+		int num1 = 100;
+		int num2 = 100;
+		
+		if(num1 == num2) {
+			System.out.println("같은값.");
+		}else {
+			System.out.println("다른 값");
+			
+		}
+		
+		String str1 = new String("홍길동");
+		
+		
+		
+		
+		
+	}
+}
